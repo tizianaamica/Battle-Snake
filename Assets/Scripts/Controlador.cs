d664af7709d08d62f8321de2ff887d6868aa5f60
Tiziana Amicarella <tizianaamicarella.tag@gmail.com>
2019-05-15 20:34:38 -0430

    first commit

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour {
+	public void CambiarEscena(string nombre){
+		print ("Cambiando a la escena" + nombre);
+		SceneManager.LoadScene (nombre);
+	}
+
+	public void Salir(){
+		print ("Saliendo del juego");
+		Application.Quit ();
+	}
+}
